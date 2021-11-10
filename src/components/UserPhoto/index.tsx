166a266d88132848d440fe1b2a5ac4a3d45e5dc8
Luiz Felipe de Oliveira <luiz.pepe@hotmail.com>
2021-11-10 18:59:45 -0300

    Trilha React Native

---
+++ src/components/UserPhoto/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { Image } from 'react-native';
+
+import { styles } from './styles';
+
+const SIZES = {
+    SMALL: {
+        containerSize: 32,
+        avatarSize: 28,
+    },
+    NORMAL: {
+        containerSize: 48,
+        avatarSize: 42,
+    }
+}
+
+type Props = {
+    imageUri: string | undefined;
+    sizes?: 'SMALL' | 'NORMAL';
+}
+
+export function UserPhoto({ imageUri, sizes = 'NORMAL' }: Props) {
+    const { containerSize, avatarSize } = SIZES[sizes];
+    return (
+        <Image
+            source={{ uri: imageUri }}
+            style={[
+                styles.avatar,
+                {
+                    width: avatarSize,
+                    height: avatarSize,
+                    borderRadius: avatarSize / 2,
+                }
+            ]}
+        />
+    );
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+import { Header } from "../../components/Header";
+
+export function Home() {
+    return (
+        <View style={styles.container}>
+            <Header />
+        </View>
+    )
+}
